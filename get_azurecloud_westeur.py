0e9d914b63c87adb731e9da252ef4dc6b72886c2
omertb <omertugrul@gmail.com>
2021-04-07 12:01:18 +0300

    sys exit

--- get_azurecloud_westeur.py
+++ get_azurecloud_westeur.py
@@ -1,11 +1,15 @@
 import requests
 import json
+import os
+import sys
 
 requests.packages.urllib3.disable_warnings()
 AZURE_JSON = "https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20210329.json"
 
 def get_azure_json():
     response = requests.request(method="GET", url=AZURE_JSON, verify=False)
+    if response.status_code != 200 or response.text == "":
+        sys.exit(-1)
     return response.text
 
 def convert_json_to_ip_list():
@@ -23,7 +27,7 @@ def convert_json_to_ip_list():
 def main():
     ip_list = convert_json_to_ip_list()
     filename = "azurecloud.westeurope.txt"
-    print(ip_list)
+    os.chdir("/home/security/Scripts/Minemeld/DA365AzureIPRange")
     with open(filename, "w") as f:
         f.write(ip_list)
 
